04a7e265a04fad979a5f14ec99a96d6954c53d9e
NathanKlump <98673875+NathanKlump@users.noreply.github.com>
2023-04-30 17:50:38 -0400

    add navbar and thumbnail

--- client/src/App.js
+++ client/src/App.js
@@ -3,28 +3,28 @@ import { get_playlist } from './api/API';
 import 'react-h5-audio-player/lib/styles.css';
 
 function App() {
-  const [videoTitles, setVideoTitles] = useState([]);
+  const [videoData, setVideoData] = useState([]);
   const [audioElement, setAudioElement] = useState(null);
   const [currentTitle, setCurrentTitle] = useState(null);
   const [isPlaying, setIsPlaying] = useState(false);
 
-  const extractVideoTitles = responseObject => {
+  const extractVideoData = responseObject => {
     const items = responseObject.items;
-    const videoTitles = [];
-    console.log(items);
+    const videoData = [];
     for (let i = 0; i < items.length; i++) {
       const videoTitle = items[i].snippet.title;
-      videoTitles.push(videoTitle);
+      const thumbnailUrl = items[i].snippet.thumbnails.medium.url;
+      videoData.push({ title: videoTitle, thumbnail: thumbnailUrl });
     }
-    return videoTitles;
+    return videoData;
   };
 
   useEffect(() => {
     const getPlaylist = () => {
       get_playlist()
         .then(data => {
-          const titles = extractVideoTitles(data);
-          setVideoTitles(titles);
+          const videoData = extractVideoData(data);
+          setVideoData(videoData);
         })
         .catch(err => {
           console.log(err);
@@ -60,13 +60,46 @@ function App() {
     }
   };
 
+  const toggleCurrentAudio = () => {
+    if (audioElement) {
+      toggleAudio(currentTitle);
+    }
+  };
+
+  const truncateTitle = (title, maxLength = 40) => {
+    if (title.length > maxLength) {
+      return title.substring(0, maxLength) + '...';
+    }
+    return title;
+  };
+
   return (
     <div className="App">
-      {videoTitles.map((title, index) => (
-        <div key={index} onClick={() => toggleAudio(title)}>
-          <p>{title}</p>
+      <nav className="bg-gray-800 p-4 flex justify-between items-center">
+        <div className="text-white font-bold">
+          {currentTitle ? `Now playing: ${currentTitle}` : 'No song playing'}
         </div>
-      ))}
+        <button
+          onClick={toggleCurrentAudio}
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+        >
+          {isPlaying ? 'Pause' : 'Resume'}
+        </button>
+      </nav>
+      {videoData.map((video, index) => (
+      <div
+        key={index}
+        className="video-item flex items-center space-x-4 cursor-pointer"
+        onClick={() => toggleAudio(video.title)}
+      >
+        <img
+          src={video.thumbnail}
+          alt={video.title}
+          className="w-8 h-8 object-cover"
+        />
+        <p>{truncateTitle(video.title)}</p>
+      </div>
+    ))}
     </div>
   );
 }
