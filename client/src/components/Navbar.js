935f3a2925e8d7960a6e9cb381079b050fb5436a
Nathaniel Klump <nathanieljklump@gmail.com>
2023-05-03 13:24:59 -0400

    style autoplay

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -14,7 +14,7 @@ function Navbar(props) {
     toggleAudio,
     videoData,
     isNextSongLoading,
-    setIsNextSongLoading
+    setIsNextSongLoading,
   } = props;
 
   const truncateTitle = (title, maxLength = 40) => {
@@ -33,8 +33,22 @@ function Navbar(props) {
               ? `Now playing: ${truncateTitle(currentTitle)}`
               : "No song playing"}
           </div>
-          <button onClick={toggleAutoplay} className="text-white font-bold">
-            {autoplay ? <p>On</p> : <p>Off</p>}
+          <button onClick={toggleAutoplay} className="font-bold">
+            {autoplay ? (
+              <svg width="24" height="24" viewBox="0 0 24 24">
+                <path
+                  fill="#FFFFFF"
+                  d="m19 8-4 4h3c0 3.31-2.69 6-6 6-1.01 0-1.97-.25-2.8-.7l-1.46 1.46C8.97 19.54 10.43 20 12 20c4.42 0 8-3.58 8-8h3l-4-4zM6 12c0-3.31 2.69-6 6-6 1.01 0 1.97.25 2.8.7l1.46-1.46C15.03 4.46 13.57 4 12 4c-4.42 0-8 3.58-8 8H1l4 4 4-4H6z"
+                ></path>
+              </svg>
+            ) : (
+              <svg width="24" height="24" viewBox="0 0 24 24">
+                <path
+                  fill="#2f2f2f"
+                  d="m19 8-4 4h3c0 3.31-2.69 6-6 6-1.01 0-1.97-.25-2.8-.7l-1.46 1.46C8.97 19.54 10.43 20 12 20c4.42 0 8-3.58 8-8h3l-4-4zM6 12c0-3.31 2.69-6 6-6 1.01 0 1.97.25 2.8.7l1.46-1.46C15.03 4.46 13.57 4 12 4c-4.42 0-8 3.58-8 8H1l4 4 4-4H6z"
+                ></path>
+              </svg>
+            )}
           </button>
           <div className="flex items-center space-x-4">
             <svg
