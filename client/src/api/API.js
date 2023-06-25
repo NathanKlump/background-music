8fcd45a68fbaa0bd545fe6ac4d52550c78e76fca
Nathaniel Klump <nathanieljklump@gmail.com>
2023-06-25 13:43:23 -0400

    Revert "add callchain for yt api"
    This reverts commit d2757bb060690c4d0162f15d41636ed1e3089e94.

--- client/src/api/API.js
+++ client/src/api/API.js
@@ -1,62 +1,36 @@
 import axios from 'axios';
 
-const base_api = {
+const playlist_api = {
   method: 'GET',
+  url: 'https://youtube-v31.p.rapidapi.com/playlistItems',
+  params: {
+    playlistId: 'PLeRj4b8xSv41Lv9dJ1I7Mk8RgQ3Kv1YC_',
+    part: 'snippet',
+    maxResults: '500',
+  },
   headers: {
     'content-type': 'application/octet-stream',
     'X-RapidAPI-Key': 'a82d08bb97msheba6e9257f802b0p163ca7jsn0392ca4ca0a3',
+    'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com',
   },
 };
+export const get_playlist = async () => {
+  const { data } = await axios.request(playlist_api);
+  return data;
+};
 
-// Function to get the list of videos from a playlist. Uses the YouTube Data API.
-export const get_playlist = async (pageToken) => {
-  const { data } = await axios.request({
-    ...base_api,
-    url: 'https://youtube-v31.p.rapidapi.com/playlistItems',
-    params: {
-      playlistId: 'PLeRj4b8xSv41Lv9dJ1I7Mk8RgQ3Kv1YC_',
-      part: 'snippet',
-      maxResults: '50',
-      pageToken: pageToken,
-    },
-    headers: {
-      ...base_api.headers,
-      'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com',
-    },
-  });
-
-  if (data.nextPageToken) {
-    const nextPageData = await get_playlist(data.nextPageToken);
-    data.items = data.items.concat(nextPageData.items);
+const download_link = {
+  method: 'GET',
+  url: 'https://youtube-mp36.p.rapidapi.com/dl',
+  params: {id: 'UxxajLWwzqY'},
+  headers: {
+    'content-type': 'application/octet-stream',
+    'X-RapidAPI-Key': 'a82d08bb97msheba6e9257f802b0p163ca7jsn0392ca4ca0a3',
+    'X-RapidAPI-Host': 'youtube-mp36.p.rapidapi.com'
   }
-
-  return data;
 };
 
-// Function to get the download link for a video. Uses the YouTube MP3 download API.
-export const get_download_link = async (videoId) => {
-  const { data } = await axios.request({
-    ...base_api,
-    url: 'https://youtube-mp36.p.rapidapi.com/dl',
-    params: {id: videoId},
-    headers: {
-      ...base_api.headers,
-      'X-RapidAPI-Host': 'youtube-mp36.p.rapidapi.com'
-    },
-  });
-
+export const get_download_link = async () => {
+  const { data } = await axios.request(download_link);
   return data;
 };
-
-// Call functions
-get_playlist().then((data) => {
-  console.log(data);
-}).catch((error) => {
-  console.error(error);
-});
-
-get_download_link('UxxajLWwzqY').then((data) => {
-  console.log(data);
-}).catch((error) => {
-  console.error(error);
-});
