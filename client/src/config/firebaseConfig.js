216bc7f2c849bb7c299b989b01c5904a96574fac
Nathaniel Klump <nathanieljklump@gmail.com>
2023-07-03 17:33:20 -0400

    opsec

--- client/src/config/firebaseConfig.js
+++ client/src/config/firebaseConfig.js
@@ -1,15 +1,14 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDVcqi-qj2UIjQMTH3oJp8PeAbMDmqmPBc",
-  authDomain: "background-music-55846.firebaseapp.com",
-  projectId: "background-music-55846",
-  storageBucket: "background-music-55846.appspot.com",
-  messagingSenderId: "143941165439",
-  appId: "1:143941165439:web:72fa907a326743ef0dfa4a",
-  measurementId: "G-DQM2TLKVRW"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
